53060adc5e1ae610193616302b6dca64aa9fe914
Kazuya Takei <myself@attakei.net>
2025-01-10 04:38:48 +0900

    feat: Simple inject navigation button into all html pages

--- src/atsphinx/goto_top/__init__.py
+++ src/atsphinx/goto_top/__init__.py
@@ -1,11 +1,42 @@
 """Add navigation buttion that scroll to top of page."""
 
+from pathlib import Path
+from typing import Optional
+
+from docutils import nodes
 from sphinx.application import Sphinx
+from sphinx.config import Config
 
 __version__ = "0.0.0"
 
+here = Path(__file__).parent
+
+
+def register_config(app: Sphinx, config: Config):
+    """Add config values for using extension."""
+    config.templates_path.append(str(here / "templates"))
+    config.html_static_path.append(str(here / "static"))
+    config.html_js_files.append("goto-top/main.js")
+    config.html_css_files.append("goto-top/style.css")
+
+
+def append_template_element(
+    app: Sphinx,
+    pagename: str,
+    templatename: str,
+    context: dict,
+    doctree: Optional[nodes.document],
+) -> None:
+    """Inject <template> into metadata."""
+    context.setdefault("metatags", "")
+    context["metatags"] += app.builder.templates.render(
+        "goto-top/navigation.html", context
+    )
+
 
 def setup(app: Sphinx):  # noqa: D103
+    app.connect("config-inited", register_config)
+    app.connect("html-page-context", append_template_element)
     return {
         "version": __version__,
         "env_version": 1,
--- tests/test_it.py
+++ tests/test_it.py
@@ -1,6 +1,7 @@
 """Standard tests."""
 
 import pytest
+from bs4 import BeautifulSoup  # type: ignore[import-untyped]
 from sphinx.testing.util import SphinxTestApp
 
 
@@ -8,3 +9,7 @@ from sphinx.testing.util import SphinxTestApp
 def test__it(app: SphinxTestApp):
     """Test to pass."""
     app.build()
+    assert (app.outdir / "_static" / "goto-top/main.js").exists()
+    assert (app.outdir / "_static" / "goto-top/style.css").exists()
+    soup = BeautifulSoup((app.outdir / "index.html").read_text(), "html.parser")
+    assert soup.find("template", {"id": "tmpl_gotoTop"})
